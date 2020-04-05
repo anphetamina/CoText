863ededb63f648c3239983afc230774699cc10a5
marco_head <s265861@studenti.polito.it>
2020-04-05 21:46:51 +0200

    MainWindow.ui updates, MainWindow.cpp/.h updates.

--- client/MainWindow.cpp
+++ client/MainWindow.cpp
@@ -1,83 +1,130 @@
 #include "MainWindow.h"
 #include "ui_mainwindow.h"
 #include "Login.h"
+//#include "dbConf.h"
 
 
 #include <QPixmap> //allows to create a qpixmap onj which takes 1 arg
-
+#include <QPrinter>
+#include <QColorDialog>
+#include <QColor>
+#include <QCloseEvent>
+#include <QtSvg>
 
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow){
-    ui->setupUi(this);
-    //this->setCentralWidget(ui->textEdit);
-    QPixmap icon(":/appIcon/CoText.ico");
-    //ui->iconLabel->setPixmap(icon);
-   // ui->statusbar->addPermanentWidget();
-
-
-
-            
-
-
+	QApplication::instance()->setAttribute(Qt::AA_DontShowIconsInMenus, true);
+	ui->setupUi(this);
+	//this->setCentralWidget(ui->textEdit);
+	QPixmap icon(":/appIcon/qPDS.ico");
+	//ui->iconLabel->setPixmap(icon);
+	// ui->statusbar->addPermanentWidget();
+	
 }
 
+
 MainWindow::~MainWindow(){
-     delete ui;
+	delete ui;
+}
+
+void MainWindow::closeEvent(QCloseEvent *event) {
+	QMessageBox::StandardButton resBtn = QMessageBox::question(this, "CoText", tr("Are your sure to quit?\n"), QMessageBox::Cancel | QMessageBox::No | QMessageBox::Yes, QMessageBox::Yes);
+	if(resBtn != QMessageBox::Yes) {
+		event->ignore();
+	} else {
+		event->accept();
+	}
 }
 
 
 void MainWindow::on_actionNew_triggered() {
-    currentFile.clear();
-    setWindowTitle("untitled");
-    ui->textEdit->setText(QString());
-
+	currentFileName.clear();
+	setWindowTitle("untitled");
+	ui->textEdit->setText(QString());
+	
 }
 
 void MainWindow::on_actionOpen_triggered() {
-    QString filename = QFileDialog::getOpenFileName(this, "Choose file to open", "/home", "All files .*");
-    if(!filename.isNull()) {
-
-        QFile file(filename);
-        QString filePath = filename.toUtf8();
-
-        currentFile = filename;
-
-        //checks
-        if(!file.open(QIODevice::ReadOnly | QFile::Text)) {
-            QMessageBox::warning(this, "Warning", "Can not open file:  "+ file.errorString());
-            return;
-        }
-
-        setWindowTitle((filename));
-        QTextStream in(&file);
-        QString text = in.readAll();
-        ui->textEdit->setText(text);
-        file.close();
-
-    } else {
-        //QMessageBox::warning(this, "Warning - filename Null", "The file selected is invalid");
-        return;
-    }
-
+	QString filename = QFileDialog::getOpenFileName(this, "Choose file to open", "/home", "All files .*");
+	if(!filename.isNull()) {
+		
+		QFile file(filename);
+		QString filePath = filename.toUtf8();
+		
+		currentFileName = filename;
+		
+		//checks
+		if(!file.open(QIODevice::ReadOnly | QFile::Text)) {
+			QMessageBox::warning(this, "Warning", "Can not open file:  "+ file.errorString());
+			return;
+		}
+		
+		setWindowTitle((filename));
+		QTextStream in(&file);
+		QString text = in.readAll();
+		ui->textEdit->setText(text);
+		file.close();
+		
+	} else {
+		//QMessageBox::warning(this, "Warning - filename Null", "The file selected is invalid");
+		return;
+	}
+	
 }
 
+void MainWindow::Save_as() {
+	
+	QString filename = QFileDialog::getSaveFileName(this, "Save as");
+	QFile file(filename);
+	if(!file.open(QIODevice::WriteOnly | QFile::Text)) {
+		QMessageBox::warning(this, "Warning", "Can not save the file:  "+ file.errorString() + ". Choose a name and save.");
+		return;
+	}
+	currentFileName = filename;
+	setWindowTitle(filename);
+	QTextStream out(&file);
+	QString text = ui->textEdit->toPlainText();
+	out << text;
+	file.close();
+	return;
+	
+	
+}
 void MainWindow::on_actionSave_as_triggered() {
-    QString filename = QFileDialog::getSaveFileName(this, "Save as");
-    QFile file(filename);
-
-    //checks
-    if(!file.open(QIODevice::WriteOnly | QFile::Text)) {
-        QMessageBox::warning(this, "Warning", "Can not save the file:  "+ file.errorString());
-        return;
-    }
-    currentFile = filename;
-    setWindowTitle(filename);
-    QTextStream out(&file);
-    QString text = ui->textEdit->toPlainText();
-    out << text;
-    file.close();
+	Save_as();
+	return;
+}
 
 
+
+void MainWindow::on_actionSave_triggered() {
+	QString filename = currentFileName;
+	QFile file(filename);
+	
+	if(!file.open(QIODevice::WriteOnly | QFile::Text)) {
+		QMessageBox::warning(this, "Warning", "Can not save the file:  "+ file.errorString()+ ". Choose a name and save.");
+		Save_as();
+		return;
+	} else {
+		QTextStream out(&file);
+		QString text = ui->textEdit->toPlainText();
+		out << text;
+		file.close();
+		return;
+	}
+}
+
+
+
+void MainWindow::on_actionPrintPDF_triggered() {
+	QPrinter printer;
+	printer.setOutputFormat(QPrinter::PdfFormat);
+	QString filename = QFileDialog::getSaveFileName(this, "Export PDF");
+	printer.setOutputFileName(filename);
+	
+	
+	
+	
 }
 /*
 void MainWindow::on_actionPrint_triggered() {
@@ -98,66 +145,68 @@ void MainWindow::on_actionPrint_triggered() {
 */
 
 void MainWindow::on_actionExit_triggered() {
-    QMessageBox::StandardButton reply = QMessageBox::question(this, "Exit", "Do you really want to quit the application?", QMessageBox::Yes | QMessageBox::No);
-
-
-    if(reply == QMessageBox::Yes) {
-        QApplication::quit();
-    } else {
-        return;
-    }
-
+	QMessageBox::StandardButton reply = QMessageBox::question(this, "Exit", "Do you really want to quit the application?", QMessageBox::Yes | QMessageBox::No);
+	
+	
+	if(reply == QMessageBox::Yes) {
+		QApplication::quit();
+	} else {
+		return;
+	}
+	
 }
 
 void MainWindow::on_actionCopy_triggered() {
-    ui->textEdit->copy();
-
+	ui->textEdit->copy();
+	
 }
 
 void MainWindow::on_actionPaste_triggered() {
-    ui->textEdit->paste();
+	ui->textEdit->paste();
 }
 
 void MainWindow::on_actionCut_triggered() {
-
-    ui->textEdit->cut();
+	
+	ui->textEdit->cut();
 }
 
 void MainWindow::on_actionUndo_triggered() {
-    ui->textEdit->undo();
+	ui->textEdit->undo();
 }
 
 void MainWindow::on_actionRedo_triggered() {
-    ui->textEdit->redo();
+	ui->textEdit->redo();
 }
 
 
 
 void MainWindow::on_actionLogin_triggered()
 {
-    //Modal approach to create loginForm (cannot access mainWindow at same time)
-    //It is on the stack, if we want it on the heap just have the pointer on mainwindow.h and call new here
-    //Connecting to MySql Database
-    /*
-    if(!dbConfigure()) {
-        QMessageBox::information(this, "DB-Connection", "Database not connected");
-    } else {
-        QMessageBox::information(this, "DB-Connection", "Database connected");
-        Login login;
-        login.setModal(true);
-        login.exec();
-    }
-    */
-
-    Login login;
-    login.setModal(true);
-    login.exec();
-
-
-    //hide(); //hide the MainWindow
-
-    //lf = new loginform(this); //giving also the parent class
-
-    //lf->show();
-
+	//Modal approach to create loginForm (cannot access mainWindow at same time)
+	//It is on the stack, if we want it on the heap just have the pointer on mainwindow.h and call new here
+	//Connecting to MySql Database
+	/*
+	if(!dbConfigure()) {
+		QMessageBox::information(this, "DB-Connection", "Database not connected");
+	} else {
+		QMessageBox::information(this, "DB-Connection", "Database connected");
+		Login login;
+		login.setModal(true);
+		login.exec();
+	}
+	*/
+	
+	Login login;
+	login.setModal(true);
+	login.exec();
+	
+	
+	//hide(); //hide the MainWindow
+	
+	//lf = new loginform(this); //giving also the parent class
+	
+	//lf->show();
+	
 }
+
+
--- client/MainWindow.h
+++ client/MainWindow.h
@@ -13,6 +13,14 @@
 
 //#include <QSqlDatabase>
 
+/* COLOR PALETTE CODE:
+    (30, 30, 30) dark
+    (37, 37, 37) greydark
+    (51, 51, 51) grey light
+    (0, 99, 161) blue buttons
+
+
+ */
 
 
 
@@ -22,37 +30,46 @@ QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	
+	MainWindow(QWidget *parent = nullptr);
+	~MainWindow();
+
+
 
 private slots:
-    void on_actionNew_triggered();
-
-    void on_actionOpen_triggered();
-
-    void on_actionSave_as_triggered();
-
-    void on_actionExit_triggered();
-
-    void on_actionCopy_triggered();
-
-    void on_actionPaste_triggered();
-
-    void on_actionCut_triggered();
-
-    void on_actionUndo_triggered();
-
-    void on_actionRedo_triggered();
-
-    void on_actionLogin_triggered();
+	void on_actionNew_triggered();
+	
+	void on_actionOpen_triggered();
+	
+	void on_actionSave_as_triggered();
+	
+	void on_actionExit_triggered();
+	
+	void on_actionCopy_triggered();
+	
+	void on_actionPaste_triggered();
+	
+	void on_actionCut_triggered();
+	
+	void on_actionUndo_triggered();
+	
+	void on_actionRedo_triggered();
+	
+	void on_actionLogin_triggered();
+	
+	void on_actionPrintPDF_triggered();
+	
+	void on_actionSave_triggered();
 
 private:
-    Ui::MainWindow *ui;
-
-
-    QString currentFile = "";
+	Ui::MainWindow *ui;
+	
+	
+	QString currentFileName = "";
+	void Save_as();
+	void closeEvent(QCloseEvent *event);
 };
 #endif // MAINWINDOW_H
