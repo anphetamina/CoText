8fd3f35de6896d6bfc5425c34907d86afbda4d45
Emanuele Munafò <ema.muna95@gmail.com>
2020-04-14 15:21:28 +0200

    Implemented user

--- client/User.h
+++ client/User.h
@@ -3,14 +3,54 @@
 //
 #pragma  once
 #include <cstdint>
+#include <QIcon>
+#include <QString>
 
 
-class User {
+#define ACTIVE 20
+#define INACTIVE 21
+#define DO_NOT_DISTURB 22
+#define OFFLINE 19
+
+
+class User  {
+
 public:
     User();
     ~User();
+    QString getName();
+    QString getSurname();
+    QString getEmail();
+    QString getNick();
+    QIcon getProfilePic();
 
-public:
-    uint8_t signature = 0x00;
+    void setName(QString _name);
+    void setSurname(QString _surname);
+    void setProfilePic(QIcon _profilePic);
+    void setNick(QString _nickname);
+    void setEmail(QString _email);
+    void addFriend(User * _usrFriend);
+    void setTimeBeforeInactive(int _timer);
+    void setUserState(int _state);
+
+    QList<User*> getFriends();
+
+    int getUserState();
+    int getActualSecondsBeforeInactive();
+
+private:
+    QList<User*> friendList;
+    QString name;
+    QString surname;
+    QString nickname;
+    QString email;
+    QIcon profilePic;
+    int userState;
+    int secondsBeforeInactive;
+    uint8_t _signature = 0x00;
+
+    void setSignature(uint8_t signature);
+
+    int getSignature();
 };
 
--- client/main.cpp
+++ client/main.cpp
@@ -3,32 +3,31 @@
 
 #include <QApplication> //manages: widgets, events, mouse movements, overall looking feel
 
+//#include <QtCore/QCoreApplication>
+#include "sslechoclient.h"
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
 
-    MainWindow *w = new MainWindow();
 
+    // ** Network testing code
+    //QCoreApplication a(argc, argv);
+    SslEchoClient client(QUrl(QStringLiteral("wss://localhost:1234")));
+    Q_UNUSED(client);
+    // ** End of network testing code
+
+    // Set GUI options
+    MainWindow *w = new MainWindow();
     w->setWindowTitle("Welcome");
     w->setWindowIconText("Co-Text");
     //w->setWindowIcon(QIcon(":/CoText.ico"));
-
-
-
     //QPixmap pm(":/CoText.ico");
-
     ////DO SOME RESIZING
     ////TODO
 
-
-
-
-
-
-
     w->show();
-
 
     return a.exec();
 }
