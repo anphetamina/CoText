d5ebe77a4c336622c3bfa5aa057698946f4b1c53
Emanuele Munafò <ema.muna95@gmail.com>
2020-08-14 11:13:36 +0200

    Update gitignore

---
+++ server/DBAuthData.h
@@ -0,0 +1,23 @@
+
+#ifndef DBCONF_H
+#define DBCONF_H
+
+#endif // DBCONF_H
+
+#include <QSqlDatabase>
+#include <QtCore>
+#include "User.h"
+
+extern QString HostName;
+extern QString UserName;
+extern QString dbPass;
+extern QString dbName;
+extern int port;
+
+bool dbConfigure();
+bool getUserlist();
+User checkLoginData(QString email, QString password);
+User* checkUserLoginData(QString email, QString password);
+QIcon loadProfilePic(int id);
+QList<QString> getDocuments(int userId);
+bool checkDocPermission(int docId, int userId);
