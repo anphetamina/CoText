7d4710ad22a9f242af74b557ad64abc905682d92
Emanuele Munafo <ema.muna95@gmail.com>
2020-10-02 13:54:33 +0200

    Added BigMEssagea and alignment packet

--- common/Message.h
+++ common/Message.h
@@ -26,3 +26,52 @@ protected:
     void readPayload(QDataStream& stream) override;
 
 };
+
+class BigMessage : public Packet {
+    friend PacketBuilder;
+private:
+    int type;
+    QVector<QSymbol> qss;
+    int siteId;
+
+public:
+    BigMessage(int type, QVector<QSymbol> qss, int siteId);
+
+    int getType() const;
+
+    QVector<QSymbol> getQSS() const;
+
+    int getSiteId() const;
+
+protected:
+    BigMessage();
+    void writePayload(QDataStream& stream) const override;
+    void readPayload(QDataStream& stream) override;
+
+};
+
+class AlignMessage : public Packet {
+
+    friend PacketBuilder;
+private:
+    int positionStart;
+    int delta = 0;
+    Qt::Alignment alignment;
+    int siteId;
+
+public:
+    AlignMessage(int positionStart, int delta, Qt::Alignment alignment, int siteId);
+
+    int getPositionStart() const;
+    int getDelta() const;
+
+    Qt::Alignment getAlignment() const;
+
+    int getSiteId() const;
+
+protected:
+    AlignMessage();
+    void writePayload(QDataStream& stream) const override;
+    void readPayload(QDataStream& stream) override;
+
+};
